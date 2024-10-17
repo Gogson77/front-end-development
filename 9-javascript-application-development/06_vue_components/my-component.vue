1d6a64f7f162513c820faf41a99785c29cadeaa8
Gordan Kranjcic <gordankranjcic@gmail.com>
2024-10-17 13:15:07 +0200

    gradivo

---
+++ 9-javascript-application-development/06_vue_components/my-component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-container">
+    <h3>{{ user.name }}</h3>
+    <p>{{ user.email }}</p>
+    <p class="balance">{{ user.balance }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: {
+    user: Object 
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  text-decoration: underline;
+}
+
+.balance {
+  font-style: italic;
+}
+</style>
